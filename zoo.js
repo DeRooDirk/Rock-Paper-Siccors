d042db2a6f1a19d575169eb25479306a149ea507
Zoo Wie <de-roo.dirk@hotmail.com>
2020-11-19 23:06:33 +0100

    update html
    added bg to main page

--- zoo.js
+++ zoo.js
@@ -1,5 +1,5 @@
 const moves = ["Rock ", "Paper ", "Scissors "],
-    messages = ["You won! ✨", "Skynet won 🧨", "It's a draw! 🎃"], // [Player, skynet, draw]
+    messages = ["John Conner wins! ✨", "Skynet wins 🧨", "It's a draw! 🎃"], // [Player, skynet, draw]
     score = [0, 0, 0], // [PL, skynet draw]
     appel = sel => document.querySelector(sel),
     appel_result = appel("#result"),
@@ -17,7 +17,7 @@ function game() {
     const result = player === skynet ? 2 : (skynet + 1) % 3 === player ? 0 : 1; // 0=PLwins 1=skynetwins 2=draw 
 
     score[result]++; // Increment PL or skynets score (Increments number of draws too ;) )
-    appel_result.innerHTML = `You: ${moves[player]}<br>skynet: ${moves[skynet]}<br>${messages[result]}`;
+    appel_result.innerHTML = `John Conner: ${moves[player]}<br>skynet: ${moves[skynet]}<br>${messages[result]}`;
     appel_playerScore.textContent = score[0];
     appel_skynetScore.textContent = score[1];
 }
